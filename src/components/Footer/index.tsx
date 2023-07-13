02c9006e5ae75237cb076ff0864d15451fdeecc5
Urban Vidovič <urbanfoundit@gmail.com>
2023-07-13 16:57:47 +0200

    fix: infinite loading on mailto link click
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,22 +6,20 @@ const Footer = () => {
     <div className="flex h-20 bg-masca-100 p-4">
       <div className="flex-1 self-center align-middle">
         <p className="">Blockchain Lab:UM</p>
-        <Link
-          className="hover:underline active:opacity-50"
+        <a
+          className="text-slate-400 hover:underline active:opacity-50"
           href="mailto:blockchain-lab@um.si"
         >
-          <p className="text-slate-400">blockchain-lab@um.si</p>
-        </Link>
+          blockchain-lab@um.si
+        </a>
       </div>
       <div className="flex flex-1 items-center justify-center self-center">
         <Link
-          className="hover:opacity-80 active:opacity-50"
+          className="text-lg font-bold text-white hover:underline hover:opacity-80 active:opacity-50"
           href="https://docs.masca.io/"
           target="_blank"
         >
-          <p className="text-lg font-bold text-slate-400 hover:underline">
-            Integrate now
-          </p>
+          Integrate now
         </Link>
       </div>
       <div className="flex flex-1 items-center justify-end">
